924607bc559eb24a817c6e9b2ca58327a3215c38
imgonnago <whdydwo0431@naver.com>
2026-04-05 03:45:47 +0900

    making main

--- run/SNP_self_attention_MLP.py
+++ run/SNP_self_attention_MLP.py
@@ -7,12 +7,25 @@ import numpy as np
 
 def SNP_main():
     # 모델 초기화
+    print("Initializing model...")
     model = SNPClassifier()
+    print("Model initialized successfully")
     # 모델 요약 출력
+    print("Model architecture:")
     print(model)
+    print("-" * 50)
     # 데이터 로더 생성
-    train_loader = get_dataloader(SNPDataset('./biogenetic', split='train'), batch_size=16, split='train')
-    val_loader = get_dataloader(SNPDataset('./biogenetic', split='val'), batch_size=16, split='val')
-    test_loader = get_dataloader(SNPDataset('./biogenetic', split='test'), batch_size=16, split='test')
+    print("Creating DataLoader...")
+    train_x, train_y = SNPDataset(data_dir='./biogenetic', split='train')
+    test_x, test_y = SNPDataset(data_dir='./biogenetic', split='test')
+    val_x, val_y = SNPDataset(data_dir='./biogenetic', split='val')
 
+    train_x = get_dataloader(train_x, batch_size=16, split='train')
+    test_x = get_dataloader(test_x, batch_size=16, split='test')
+    val_x = get_dataloader(val_x, batch_size=16, split='val')
+
+    train_y = get_dataloader(train_y, batch_size=16, split='train')
+    test_y = get_dataloader(test_y, batch_size=16, split='test')
+    val_y = get_dataloader(val_y, batch_size=16, split='val')
+    print("DataLoader created successfully")
     
